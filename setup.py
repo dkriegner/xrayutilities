8f5e704cf139186bde87e5391dada098a755aeeb
Dominik Kriegner <dominik.kriegner@aol.at>
2012-02-03 14:31:47 +0100

    correct wrong copyright notices(updated projectname)

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # This file is part of xrayutilities.
 #
-# xrutils is free software; you can redistribute it and/or modify 
+# xrayutilies is free software; you can redistribute it and/or modify 
 # it under the terms of the GNU General Public License as published by 
 # the Free Software Foundation; either version 2 of the License, or 
 # (at your option) any later version.
