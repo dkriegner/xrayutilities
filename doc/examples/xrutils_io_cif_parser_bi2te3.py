b21b5acd94f0f041dc2f52c35844d0e65c321b2f
Dominik Kriegner <dominik.kriegner@gmail.com>
2013-01-30 17:02:44 +0100

    fix bug in CIF-parser example and simplify

--- doc/examples/xrutils_io_cif_parser_bi2te3.py
+++ doc/examples/xrutils_io_cif_parser_bi2te3.py
@@ -20,10 +20,10 @@ import numpy
 import os
 
 # parse cif file to get unit cell structure
-ciff = xu.io.CIFFile(os.path.join("data","bi2te3.cif")
+ciff = xu.io.CIFFile(os.path.join("data","bi2te3.cif"))
 
 #create material
-bite = xu.materials.Material("Bi2Te3",ciff.Lattice(),numpy.zeros((6,6),dtype=numpy.double))
+bite = xu.materials.Material("Bi2Te3",ciff.Lattice())
 
 #structure factor calculation
 # bite= bite.StructureFactor(bite.Q(h,k,l),energy)
