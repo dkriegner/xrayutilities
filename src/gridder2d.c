b7f70f43c9e0b3a07a524385c98cd5745bcc3e5b
Dominik Kriegner <dominik.kriegner@gmail.com>
2013-06-10 00:33:16 +0200

    fix warnings during build due to deprecated funcs

--- src/gridder2d.c
+++ src/gridder2d.c
@@ -23,6 +23,7 @@
  * author: Eugen Wintersberger
 */
 
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #define PY_ARRAY_UNIQUE_SYMBOL XU_UNIQUE_SYMBOL
 #include "gridder.h"
 #include "gridder_utils.h"
