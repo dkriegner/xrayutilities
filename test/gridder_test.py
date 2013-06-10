6413285c0bc1ff24121b77fb87d6176750147843
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2013-06-10 23:48:50 +0200

    Changed test for gridder.

--- test/gridder_test.py
+++ test/gridder_test.py
@@ -12,7 +12,8 @@ y = x
 nx = x.size
 ny = y.size
 
-data = numpy.random.random_sample(x.size)
+#data = numpy.random.random_sample(x.size)
+data = numpy.array([1,2,3,4,5,6,7,8,9,10],dtype="float64")
 out = numpy.zeros((nx,ny),dtype="float64")
 
 cu.gridder2d(x,y,data,nx,ny,xmin,xmax,ymin,ymax,out)
