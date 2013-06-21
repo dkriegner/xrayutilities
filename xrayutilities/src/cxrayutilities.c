5b7d60f4526d03171c2873e4aaf60d2c227ebb34
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2013-06-22 00:04:27 +0200

    Fix C code for 3d gridder.

--- xrayutilities/src/cxrayutilities.c
+++ xrayutilities/src/cxrayutilities.c
@@ -33,6 +33,9 @@ extern PyObject* block_average_PSD(PyObject *self, PyObject *args);
 /* functions from gridder2d.c */
 extern PyObject* pygridder2d(PyObject *self,PyObject *args);
 
+/* function from gridder3d.c */
+extern PyObject* pygridder3d(PyObject *self,PyObject *args);
+
 /* functions from qconversion.c */
 extern PyObject* ang2q_conversion(PyObject *self, PyObject *args);
 extern PyObject* ang2q_conversion_linear(PyObject *self, PyObject *args);
@@ -94,6 +97,25 @@ static PyMethodDef XRU_Methods[] = {
      "  xmax ... maximum x-value of the grid\n"
      "  ymin ... minimum y-value of the grid\n"
      "  ymax ... minimum y-value of the grid\n"
+     "  out .... output data\n"
+    },
+    {"gridder3d",pygridder3d,METH_VARARGS, 
+     "Function performs 2D gridding on 1D input data. \n\n" 
+     "Parameters\n"
+     "----------\n"
+     "  x ...... input x-values (1D numpy array - float64)\n"
+     "  y ...... input y-values (1D numpy array - float64)\n"
+     "  z ...... input z-values (1D numpy array - float64)\n"
+     "  data ... input data (1D numpy array - float64)\n"
+     "  nx ..... number of grid points in x-direction\n"
+     "  ny ..... number of grid points in y-direction\n"
+     "  nz ..... number of grid points in z-direction\n"
+     "  xmin ... minimum x-value of the grid\n"
+     "  xmax ... maximum x-value of the grid\n"
+     "  ymin ... minimum y-value of the grid\n"
+     "  ymax ... maximum y-value of the grid\n"
+     "  zmin ... minimum z-value of the grid\n"
+     "  zmax ... maximum z-value of the grid\n"
      "  out .... output data\n"
     },
     {"ang2q_conversion", ang2q_conversion, METH_VARARGS,
