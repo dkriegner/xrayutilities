d4ed94765df268cafd4c8bdec4295476738f30ae
Dominik Kriegner <dominik.kriegner@gmail.com>
2013-07-03 11:34:40 +0200

    change for new release of version 0.99.1

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ extmodul = Extension('xrayutilities.cxrayutilities',
                      define_macros = user_macros)
 
 setup(name="xrayutilities",
-      version="0.99",
+      version="0.99.1",
       author="Eugen Wintersberger, Dominik Kriegner",
       description="package for x-ray diffraction data evaluation",
       classifiers=["Topic :: Scientific/Engineering :: Physics",
