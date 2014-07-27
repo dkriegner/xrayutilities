844124194f9938d6c7d152fc1fc7baa83874396c
Dominik Kriegner <dominik.kriegner@gmail.com>
2014-07-27 17:37:06 +0200

    add primitive tests for energy scan ability

--- xrayutilities/tests/test_qconversion_area.py
+++ xrayutilities/tests/test_qconversion_area.py
@@ -8,7 +8,9 @@ class TestQConversion(unittest.TestCase):
         self.mat = xu.materials.Si
         self.hxrd = xu.HXRD(self.mat.Q(1,1,0),self.mat.Q(0,0,1))
         self.nch = (9,13)
-        self.hxrd.Ang2Q.init_area('z+','x+',4,6,self.nch[0],self.nch[1],1.0,50e-6,50e-6)
+        self.ncch1 = 4
+        self.ncch2 = 6
+        self.hxrd.Ang2Q.init_area('z+','x+',self.ncch1,self.ncch2,self.nch[0],self.nch[1],1.0,50e-6,50e-6)
         self.hklsym = (0,0,4)
         self.hklasym = (2,2,4)
 
@@ -27,6 +29,15 @@ class TestQConversion(unittest.TestCase):
         for i in range(3):
             q = qout[i]
             self.assertAlmostEqual(numpy.average(q), self.hklasym[i], places=6)
+    
+    def test_qconversion_area_energy(self):
+        ang1 = self.hxrd.Q2Ang(self.mat.Q(self.hklsym))
+        ang2 = self.hxrd.Q2Ang(self.mat.Q(self.hklsym)/2.)
+        qout = self.hxrd.Ang2HKL((ang1[0],ang2[0]),(ang1[3],ang2[3]),en=(self.hxrd.energy,2*self.hxrd.energy),mat=self.mat,dettype='area')
+        for i in range(3):
+            q = qout[i]
+            self.assertAlmostEqual(q[0,self.ncch1,self.ncch2], self.hklsym[i], places=10)
+            self.assertAlmostEqual(q[1,self.ncch1,self.ncch2], self.hklsym[i], places=10)
 
 if __name__ == '__main__':
         unittest.main()
--- xrayutilities/tests/test_qconversion_linear.py
+++ xrayutilities/tests/test_qconversion_linear.py
@@ -8,7 +8,8 @@ class TestQConversion(unittest.TestCase):
         self.mat = xu.materials.Si
         self.hxrd = xu.HXRD(self.mat.Q(1,1,0),self.mat.Q(0,0,1))
         self.nch = 9
-        self.hxrd.Ang2Q.init_linear('z+',4,self.nch,1.0,50e-6,0.)
+        self.ncch = 4
+        self.hxrd.Ang2Q.init_linear('z+',self.ncch,self.nch,1.0,50e-6,0.)
         self.hklsym = (0,0,4)
         self.hklasym = (2,2,4)
 
@@ -27,6 +28,15 @@ class TestQConversion(unittest.TestCase):
         for i in range(3):
             q = qout[i]
             self.assertAlmostEqual(numpy.average(q), self.hklasym[i], places=6)   
+
+    def test_qconversion_linear_energy(self):
+        ang1 = self.hxrd.Q2Ang(self.mat.Q(self.hklsym))
+        ang2 = self.hxrd.Q2Ang(self.mat.Q(self.hklsym)/2.)
+        qout = self.hxrd.Ang2HKL((ang1[0],ang2[0]),(ang1[3],ang2[3]),en=(self.hxrd.energy,2*self.hxrd.energy),mat=self.mat,dettype='linear')
+        for i in range(3):
+            q = qout[i]
+            self.assertAlmostEqual(q[0,self.ncch], self.hklsym[i], places=10)
+            self.assertAlmostEqual(q[1,self.ncch], self.hklsym[i], places=10)
 
 if __name__ == '__main__':
         unittest.main()
