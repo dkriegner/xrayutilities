97f4e2bb7847b63b1090beb3e532300f047f5500
Dominik Kriegner <dominik.kriegner@gmail.com>
2014-09-26 20:44:49 +0200

    update docu examples w.r.t to python3 and PEP8

--- doc/source/example_xu_ccd_parameter.py
+++ doc/source/example_xu_ccd_parameter.py
@@ -1,17 +1,20 @@
 """
-example script to show the detector parameter determination for area detectors from images recorded in the primary beam
+example script to show the detector parameter determination for area detectors
+from images recorded in the primary beam
 """
 
 import xrayutilities as xu
 import os
 
-en=10300.0 #eV
-datadir = os.path.join("data","wire_") # data path for CCD files
-filetmp = os.path.join(datadir,"wire_12_%05d.edf.gz") # template for the CCD file names
+en = 10300.0  # eV
+datadir = os.path.join("data", "wire_")  # data path for CCD files
+# template for the CCD file names
+filetmp = os.path.join(datadir, "wire_12_%05d.edf.gz")
 
-## manually selected images
+# manually selected images
 # select images which have the primary beam fully on the CCD
-imagenrs = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]
+imagenrs = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+            20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]
 
 images = []
 ang1 = []
@@ -21,7 +24,7 @@ ang2 = []
 # this might differ for data taken at different beamlines since
 # they way how motor positions are stored is not always consistent
 for imgnr in imagenrs:
-    filename = filetmp%imgnr
+    filename = filetmp % imgnr
     edf = xu.io.EDFFile(filename)
     images.append(edf.data)
     ang1.append(float(edf.header['ESRF_ID01_PSIC_NANO_NU']))
@@ -29,8 +32,10 @@ for imgnr in imagenrs:
 
 
 # call the fit for the detector parameters
-# detector arm rotations and primary beam direction need to be given
-# in total 8 parameters are fitted, however the 4 misalignment parameters can be fixed
-# they are the detector tilt azimuth, the detector tilt angle, the detector rotation around the primary beam and the outer angle offset
-param,eps = xu.analysis.sample_align.area_detector_calib(ang1,ang2,images,['z+','y-'],'x+',start=(45,0,-0.7,0),fix=(False,False,False,False),wl=xu.en2lam(en))
-
+# detector arm rotations and primary beam direction need to be given.
+# in total 8 parameters are fitted, however the 4 misalignment parameters can
+# be fixed they are the detector tilt azimuth, the detector tilt angle, the
+# detector rotation around the primary beam and the outer angle offset
+param, eps = xu.analysis.sample_align.area_detector_calib(
+    ang1, ang2, images, ['z+', 'y-'], 'x+', start=(45, 0, -0.7, 0),
+    fix=(False, False, False, False), wl=xu.en2lam(en))
--- doc/source/example_xu_linear_detector_parameters.py
+++ doc/source/example_xu_linear_detector_parameters.py
@@ -10,14 +10,14 @@ import os
 # load any data file with with the detector spectra of a reference scan
 # in the primary beam, here I use spectra measured with a Seifert XRD
 # diffractometer
-dfile = os.path.join("data","primarybeam_alignment20130403_2_dis350.nja")
+dfile = os.path.join("data", "primarybeam_alignment20130403_2_dis350.nja")
 s = xu.io.SeifertScan(dfile)
 
-ang = s.axispos["T"] # detector angles during the scan
-spectra = s.data[:,:,1] # detector spectra aquired
+ang = s.axispos["T"]  # detector angles during the scan
+spectra = s.data[:, :, 1]  # detector spectra aquired
 
 # determine detector parameters
 # this function accepts some optional arguments to describe the goniometer
 # see the API documentation
-pwidth,cch,tilt = xu.analysis.linear_detector_calib(ang,spectra,usetilt=True)
-
+pwidth, cch, tilt = xu.analysis.linear_detector_calib(ang, spectra,
+                                                      usetilt=True)
