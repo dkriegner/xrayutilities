46f11ff5a9f009ff9cc375dae072b768392e05e1
Dominik Kriegner <dominik.kriegner@gmail.com>
2014-12-10 12:05:16 +0100

    abandon minidom and use xml.etree.cElementTree
    minidom apparently has problems with memory leaks [1]. Parsing large
    XRDML-files this caused problems since the memory use was unproportional.
    
    [1] http://stackoverflow.com/questions/26787026/memory-leak-parsing-xml-using-xml-dom-minidom/27398054#27398054

--- xrayutilities/io/panalytical_xml.py
+++ xrayutilities/io/panalytical_xml.py
@@ -22,7 +22,7 @@ based on the native python xml.dom.minidom module.
 want to keep the number of dependancies as small as possible
 """
 
-from xml.dom import minidom
+from xml.etree import cElementTree as ElementTree
 import numpy
 import os
 import warnings
@@ -37,14 +37,15 @@ class XRDMLMeasurement(object):
     class to handle scans in a XRDML datafile
     """
 
-    def __init__(self, measurement):
+    def __init__(self, measurement, namespace=''):
         """
         initialization routine for a XRDML measurement which parses are all
         scans within this measurement.
         """
 
+        self.namespace = namespace
         # get scans in <xrdMeasurement>
-        slist = measurement.getElementsByTagName("scan")
+        slist = measurement.findall(self.namespace + "scan")
 
         self.ddict = {}
         is_scalar = 0
@@ -52,27 +53,26 @@ class XRDMLMeasurement(object):
         # loop over all scan entries - scan points
         for s in slist:
             # check if scan is complete
-            scanstatus = s.getAttribute("status")
+            scanstatus = s.get("status")
             if scanstatus == "Aborted" and len(slist) > 1:
                 if config.VERBOSITY >= config.INFO_LOW:
                     print("XU.io.XRDMLFile: subscan has been aborted "
                           "(part of the data unavailable)!")
             else:
-                self.scanmotname = s.getAttribute("scanAxis")
-                points = s.getElementsByTagName("dataPoints")[0]
+                self.scanmotname = s.get("scanAxis")
+                points = s.find(self.namespace + "dataPoints")
 
                 # add count time to output data
-                countTime = points.getElementsByTagName(
-                    "commonCountingTime")[0].childNodes[0].nodeValue
+                countTime = points.find(self.namespace +
+                                        "commonCountingTime").text
                 if "countTime" not in self.ddict:
                     self.ddict["countTime"] = []
                 self.ddict["countTime"].append(float(countTime))
 
                 # check for intensities first to get number of points in scan
-                det = points.getElementsByTagName("intensities")[0]
-                data = det.childNodes[0]
+                data = points.find(self.namespace + "intensities").text
                 # count time normalization; output is counts/sec
-                data_list = (numpy.fromstring(data.nodeValue, sep=" ") /
+                data_list = (numpy.fromstring(data, sep=" ") /
                              float(countTime)).tolist()
                 nofpoints = len(data_list)
                 if "detector" not in self.ddict:
@@ -81,10 +81,10 @@ class XRDMLMeasurement(object):
                 # if present read beamAttenuationFactors
                 # they are already corrected in the data file, but may be
                 # interesting
-                attfact = points.getElementsByTagName("beamAttenuationFactors")
-                if len(attfact) != 0:
-                    data = attfact[0].childNodes[0]
-                    data_list = numpy.fromstring(data.nodeValue, sep=" ")
+                attfact = points.find(self.namespace + "beamAttenuationFactors")
+                if attfact:
+                    data = attfact.text
+                    data_list = numpy.fromstring(data, sep=" ")
                     data_list = data_list.tolist()
                     nofpoints = len(data_list)
                     if "beamAttenuationFactors" not in self.ddict:
@@ -92,31 +92,27 @@ class XRDMLMeasurement(object):
                     self.ddict["beamAttenuationFactors"].append(data_list)
 
                 # read the axes position
-                pos = points.getElementsByTagName("positions")
+                pos = points.findall(self.namespace + "positions")
                 for p in pos:
                     # read axis name and unit
-                    aname = p.getAttribute("axis")
-                    aunit = p.getAttribute("unit")
+                    aname = p.get("axis")
+                    aunit = p.get("unit")
 
                     # read axis data
-                    l = p.getElementsByTagName("listPositions")
-                    s = p.getElementsByTagName("startPosition")
-                    e = p.getElementsByTagName("endPosition")
+                    l = p.findall(self.namespace + "listPositions")
+                    s = p.findall(self.namespace + "startPosition")
+                    e = p.findall(self.namespace + "endPosition")
                     if len(l) != 0:  # listPositions
                         l = l[0]
-                        data = l.childNodes[0]
-                        data_list = numpy.fromstring(data.nodeValue, sep=" ")
+                        data_list = numpy.fromstring(l.text, sep=" ")
                         data_list = data_list.tolist()
                     elif len(s) != 0:  # start endPosition
                         data_list = numpy.linspace(
-                            float(s[0].childNodes[0].nodeValue),
-                            float(e[0].childNodes[0].nodeValue),
+                            float(s[0].text), float(e[0].text),
                             nofpoints).tolist()
                     else:  # commonPosition
-                        l = p.getElementsByTagName("commonPosition")
-                        l = l[0]
-                        data = l.childNodes[0]
-                        data_list = numpy.fromstring(data.nodeValue, sep=" ")
+                        l = p.find(self.namespace + "commonPosition")
+                        data_list = numpy.fromstring(l.text, sep=" ")
                         data_list = data_list.tolist()
                         is_scalar = 1
 
@@ -182,16 +178,17 @@ class XRDMLFile(object):
 
         """
         self.filename = fname
-        d = minidom.parse(xu_open(fname))
-        root = d.childNodes[0]
+        d = ElementTree.parse(xu_open(fname))
+        root = d.getroot()
+        namespace = root.tag[:root.tag.index('}')+1]
 
-        slist = root.getElementsByTagName("xrdMeasurement")
+        slist = root.findall(namespace+"xrdMeasurement")
 
         # determine the number of scans in the file
         self.nscans = len(slist)
         self.scans = []
         for s in slist:
-            self.scans.append(XRDMLMeasurement(s))
+            self.scans.append(XRDMLMeasurement(s, namespace))
 
         if self.nscans == 1:
             self.scan = self.scans[0]
