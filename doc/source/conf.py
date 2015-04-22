57ed57f3a6693ce3aee224a9f4369ffeeb1cf0e8
Dominik Kriegner <dominik.kriegner@gmail.com>
2015-04-22 12:14:59 +0200

    update webpage design to be mobile-friendly [1]
    [1] https://www.google.com/webmasters/tools/mobile-friendly/

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,6 +14,7 @@
 
 import sys
 import os
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -102,12 +103,13 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'sidebarwidth': 280}
+html_theme_options = {'logo_only': True}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
