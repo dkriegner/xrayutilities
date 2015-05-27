9cea78ceb8065f36cb359ee71d84b4fc0755a238
Dominik Kriegner <dominik.kriegner@gmail.com>
2015-05-27 12:21:48 +0200

    add unittest for tty08File/gettty08_scan

--- xrayutilities/io/desy_tty08.py
+++ xrayutilities/io/desy_tty08.py
@@ -17,11 +17,11 @@
 
 
 """
-class for reading data+header information from tty08 data files
+class for reading data + header information from tty08 data files
 
-tty08 is system used at beamline P08 at Hasylab Hamburg and creates simple
+tty08 is a system used at beamline P08 at Hasylab Hamburg and creates simple
 ASCII files to save the data. Information is easily read from the multicolumn
-data file.  the functions below enable also to parse the information of the
+data file. the functions below enable also to parse the information of the
 header
 """
 
@@ -143,7 +143,7 @@ class tty08File(object):
         self.data = numpy.rec.fromrecords(self.data, names=self.columns)
 
 
-def gettty08_scan(scanname, scannumbers, *args):
+def gettty08_scan(scanname, scannumbers, *args, **keyargs):
     """
     function to obtain the angular cooridinates as well as intensity values
     saved in TTY08 datafiles. Especially usefull for reciprocal space map
@@ -163,6 +163,8 @@ def gettty08_scan(scanname, scannumbers, *args):
      to read reciprocal space maps measured in coplanar diffraction give:
      omname:  e.g. name of the omega motor (or its equivalent)
      ttname:  e.g. name of the two theta motor (or its equivalent)
+
+     **keyargs: keyword arguments are passed on to tty08File
 
     Returns
     -------
@@ -195,7 +197,7 @@ def gettty08_scan(scanname, scannumbers, *args):
     MAP = numpy.zeros(0)
 
     for nr in scanlist:
-        scan = tty08File(scanname % nr)
+        scan = tty08File(scanname % nr, **keyargs)
         sdata = scan.data
         if MAP.dtype == numpy.float64:
             MAP.dtype = sdata.dtype
@@ -218,5 +220,7 @@ def gettty08_scan(scanname, scannumbers, *args):
 
     if len(args) == 0:
         return MAP
+    elif len(args) == 1:
+        return retval[0], MAP
     else:
         return retval, MAP
---
+++ xrayutilities/tests/test_io_tty.py
@@ -0,0 +1,65 @@
+# This file is part of xrayutilities.
+#
+# xrayutilities is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2015 Dominik Kriegner <dominik.kriegner@gmail.com>
+
+import os.path
+import unittest
+
+import xrayutilities as xu
+import numpy
+
+testfiletmp = 'p08tty_%05d.dat'
+testfile = testfiletmp % 29
+datadir = 'data'
+fullfilename = os.path.join(datadir, testfile)
+
+
+@unittest.skipIf(not os.path.isfile(fullfilename),
+                 "additional test data needed (http://xrayutilities.sf.net)")
+class TestIO_FIO(unittest.TestCase):
+    dshape = (102,)
+    dmax = 1444999.0
+    dmin = 314133.0
+    motmax = 26.15
+    motmin = 13.0
+    tpos = 53
+    dtpos = 342586.0
+    motorname = 'om'
+    countername = 'EigerInt'
+
+    @classmethod
+    def setUpClass(cls):
+        cls.motor, cls.data = xu.io.gettty08_scan(testfiletmp, (29, 30),
+                                                  cls.motorname, path=datadir)
+        cls.inte = cls.data[cls.countername]
+
+    def test_datashape(self):
+        self.assertEqual(self.dshape, self.data.shape)
+        self.assertEqual(self.dshape, self.motor.shape)
+
+    def test_datavalues(self):
+        self.assertAlmostEqual(self.motmax, self.motor.max(), places=6)
+        self.assertAlmostEqual(self.motmin, self.motor.min(), places=6)
+        self.assertAlmostEqual(self.dmax, self.inte.max(), places=6)
+        self.assertAlmostEqual(self.dmin, self.inte.min(), places=6)
+        self.assertAlmostEqual(self.dtpos, self.inte[self.tpos], places=6)
+
+    def test_equaldata(self):
+        self.assertTrue(numpy.all(self.motor == self.data[self.motorname]))
+
+
+if __name__ == '__main__':
+    unittest.main()
