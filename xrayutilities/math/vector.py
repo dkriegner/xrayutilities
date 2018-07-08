b513fd05f2c28f58ea20044eed85c962728f292f
Dominik Kriegner <dominik.kriegner@gmail.com>
2018-07-08 14:42:01 +0200

    expand vector operations to (n, 3) vectors

--- xrayutilities/math/vector.py
+++ xrayutilities/math/vector.py
@@ -49,16 +49,20 @@ def VecNorm(v):
     Parameters
     ----------
     v :     list or array-like
-        input vector
+        input vector(s), either one vector or an array of vectors with shape
+        (n, 3)
 
     Returns
     -------
-    float
-        vector norm
+    float or ndarray
+        vector norm, either a single float or shape (n, )
     """
+    if isinstance(v, numpy.ndarray):
+        if len(v.shape) >= 2:
+            return numpy.linalg.norm(v, axis=-1)
     if len(v) != 3:
-        raise ValueError("Vector must be of size 3, but has size %d!" % len(v))
-
+        raise ValueError("Vector must be of length 3, but has length %d!"
+                         % len(v))
     return math.sqrt(v[0]**2 + v[1]**2 + v[2]**2)
 
 
@@ -69,15 +73,19 @@ def VecUnit(v):
     Parameters
     ----------
     v :     list or array-like
-        input vector
+        input vector(s), either one vector or an array of vectors with shape
+        (n, 3)
 
     Returns
     -------
     ndarray
-        unit vector of `v`
+        unit vector of `v`, either shape (3, ) or (n, 3)
     """
     vtmp = _checkvec(v)
-    return numpy.copy(vtmp) / VecNorm(vtmp)
+    if len(vtmp.shape) == 1:
+        return vtmp / VecNorm(vtmp)
+    else:
+        return vtmp / VecNorm(vtmp)[..., numpy.newaxis]
 
 
 def VecDot(v1, v2):
@@ -87,12 +95,17 @@ def VecDot(v1, v2):
     Parameters
     ----------
     v1, v2 :    list or array-like
-        input vectors
+        input vector(s), either one vector or an array of vectors with shape
+        (n, 3)
 
     Returns
     -------
-    float
+    float or ndarray
+        innter product of the vectors, either a single float or (n, )
     """
+    if isinstance(v1, numpy.ndarray):
+        if len(v1.shape) >= 2:
+            return numpy.einsum('...i, ...i', v1, v2)
     if len(v1) != 3 or len(v2) != 3:
         raise ValueError("Vectors must be of size 3! (len(v1)=%d len(v2)=%d)"
                          % (len(v1), len(v2)))
@@ -107,14 +120,19 @@ def VecCross(v1, v2, out=None):
     Parameters
     ----------
     v1, v2 :    list or array-like
-        input vectors
+        input vector(s), either one vector or an array of vectors with shape
+        (n, 3)
     out :       list or array-like, optional
         output vector
 
     Returns
     -------
-    float
+    ndarray
+        cross product either of shape (3, ) or (n, 3)
     """
+    if isinstance(v1, numpy.ndarray):
+        if len(v1.shape) >= 2 or len(v2.shape) >= 2:
+            return numpy.cross(v1, v2)
     if len(v1) != 3 or len(v2) != 3:
         raise ValueError("Vectors must be of size 3! (len(v1)=%d len(v2)=%d)"
                          % (len(v1), len(v2)))
@@ -137,21 +155,30 @@ def VecAngle(v1, v2, deg=False):
     Parameters
     ----------
     v1, v2 :    list or array-like
-        input vectors
+        input vector(s), either one vector or an array of vectors with shape
+        (n, 3)
     deg:        bool
         True: return result in degree, False: in radiants
 
     Returns
     -------
-    float
-        the angle included by the two vectors `v1` and `v2`
+    float or ndarray
+        the angle included by the two vectors `v1` and `v2`, either a single
+        float or an array with shape (n, )
     """
     u1 = VecNorm(v1)
     u2 = VecNorm(v2)
 
-    alpha = math.acos(min(1., VecDot(v1, v2) / u1 / u2))
-    if deg:
-        alpha = math.degrees(alpha)
+    if isinstance(u1, numpy.ndarray) or isinstance(u2, numpy.ndarray):
+        s = VecDot(v1, v2) / u1 / u2
+        s[s > 1.0] = 1.0
+        alpha = numpy.arccos(s)
+        if deg:
+            alpha = numpy.degrees(alpha)
+    else:
+        alpha = math.acos(min(1., VecDot(v1, v2) / u1 / u2))
+        if deg:
+            alpha = math.degrees(alpha)
 
     return alpha
 
