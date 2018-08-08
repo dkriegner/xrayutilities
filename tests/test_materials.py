63a1163e80902a639da9384f217d12c1f91c9b1d
Dominik Kriegner <dominik.kriegner@gmail.com>
2018-08-08 12:13:48 +0200

    fix isequivalent test code

--- tests/test_materials.py
+++ tests/test_materials.py
@@ -118,8 +118,8 @@ class TestMaterialsTransform(unittest.TestCase):
         hkl2lst = [((2, 1, -3), (2, 2, 3)),
                    ((1, -3, 3), (1, -2, 3)),
                    ((-2, 1, 3), (3, 2, 1)),
-                   ((1, 2, 3), (1, 3, 2)),
-                   ((-1, -2, -3), (1, 2, -3))]
+                   ((1, 3, 2), (1, 3, -2)),
+                   ((1, 2, -3), (1, 3, 2))]
         for mname, hkl2s in zip(materials, hkl2lst):
             mat = getattr(xu.materials, mname)
             self.assertTrue(mat.lattice.isequivalent(hkl1, hkl2s[0]))
