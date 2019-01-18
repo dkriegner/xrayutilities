5cdc7178921fe1d894167f9b232b66b00763616b
Dominik Kriegner <dominik.kriegner@gmail.com>
2019-01-18 19:20:25 +0100

    add new simple unittests for simulation models
    basically these tests just test if the code is running. nevertheless
    they should allow to catch regression bugs

---
+++ tests/test_simpack_dynamicalmodel.py
@@ -0,0 +1,76 @@
+# This file is part of xrayutilities.
+#
+# xrayutilities is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2019 Dominik Kriegner <dominik.kriegner@gmail.com>
+
+import unittest
+
+import numpy
+import xrayutilities as xu
+
+
+class Test_DynamicalModel(unittest.TestCase):
+    # define used layer stack
+    sub = xu.simpack.Layer(xu.materials.GaAs, numpy.inf)
+    lay = xu.simpack.Layer(xu.materials.AlGaAs(0.75), 995.64, relaxation=0.0)
+    pls = xu.simpack.PseudomorphicStack001('AlGaAs on GaAs', sub, lay)
+    hkl = (0, 0, 4)
+
+    # simulation parameters
+    kwargs = dict(I0=1e6, background=0, resolution_width=1e-5)
+
+    @classmethod
+    def setUpClass(cls):
+        cls.sdyn = xu.simpack.SimpleDynamicalCoplanarModel(cls.pls,
+                                                           **cls.kwargs)
+        cls.dyn = xu.simpack.DynamicalModel(cls.pls, **cls.kwargs)
+        cls.fms = xu.simpack.FitModel(cls.sdyn)
+        cls.fmd = xu.simpack.FitModel(cls.dyn)
+        qz = numpy.linspace(4.40, 4.50, 2000)
+        cls.ai = xu.simpack.coplanar_alphai(0, qz)
+
+    def test_Calculation(self):
+        sim = self.dyn.simulate(self.ai, hkl=self.hkl)
+        self.assertEqual(len(sim), len(self.ai))
+        self.assertTrue(numpy.all(sim >= self.kwargs['background']))
+        self.assertTrue(numpy.all(sim <= self.kwargs['I0']))
+
+    def test_FitModel_eval(self):
+        sim1 = self.sdyn.simulate(self.ai, hkl=self.hkl)
+        p = self.fms.make_params()
+        sim2 = self.fms.eval(p, x=self.ai, hkl=self.hkl)
+        for v1, v2 in zip(sim1, sim2):
+            self.assertAlmostEqual(v1, v2, places=10)
+
+    def test_FitModel_eval_dyn(self):
+        sim1 = self.dyn.simulate(self.ai, hkl=self.hkl)
+        p = self.fmd.make_params()
+        sim2 = self.fmd.eval(p, x=self.ai, hkl=self.hkl)
+        for v1, v2 in zip(sim1, sim2):
+            self.assertAlmostEqual(v1, v2, places=10)
+
+    def test_Consistency(self):
+        sim1 = self.sdyn.simulate(self.ai, hkl=self.hkl)
+        sim2 = self.dyn.simulate(self.ai, hkl=self.hkl)
+        self.assertEqual(len(sim1), len(sim2))
+        self.assertEqual(numpy.argmax(sim1), numpy.argmax(sim2))
+        self.assertAlmostEqual(xu.math.fwhm_exp(self.ai, sim1),
+                               xu.math.fwhm_exp(self.ai, sim2), places=4)
+        self.assertTrue(xu.math.fwhm_exp(self.ai, sim1) >=
+                        self.kwargs['resolution_width'])
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_simpack_xrrspecular.py
@@ -0,0 +1,65 @@
+# This file is part of xrayutilities.
+#
+# xrayutilities is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2019 Dominik Kriegner <dominik.kriegner@gmail.com>
+
+import unittest
+
+import numpy
+import xrayutilities as xu
+
+
+class Test_SpecularReflectivityModel(unittest.TestCase):
+    # define used layer stack
+    lSiO2 = xu.simpack.Layer(xu.materials.SiO2, numpy.inf, roughness=3.0)
+    lRu = xu.simpack.Layer(xu.materials.Ru, 50, roughness=2.5)
+    rho_cf = 0.5*8900 + 0.5*7874
+    mat_cf = xu.materials.Amorphous('CoFe', rho_cf)
+    lCoFe = xu.simpack.Layer(mat_cf, 30, roughness=5.2)
+
+    # simulation parameters
+    kwargs = dict(I0=1e6, background=2, sample_width=10, beam_width=0.2,
+                  energy='MoKa1')
+
+    @classmethod
+    def setUpClass(cls):
+        cls.model = xu.simpack.SpecularReflectivityModel(cls.lSiO2, cls.lRu,
+                                                         cls.lCoFe,
+                                                         **cls.kwargs)
+        cls.fm = xu.simpack.FitModel(cls.model)
+        cls.ai = numpy.arange(0.1, 3, 0.005)
+
+    def test_Calculation(self):
+        sim = self.model.simulate(self.ai)
+        self.assertEqual(len(sim), len(self.ai))
+        self.assertTrue(numpy.all(sim >= self.kwargs['background']))
+        self.assertTrue(numpy.all(sim <= self.kwargs['I0']))
+
+    def test_FitModel_eval(self):
+        sim1 = self.model.simulate(self.ai)
+        p = self.fm.make_params()
+        sim2 = self.fm.eval(p, x=self.ai)
+        for v1, v2 in zip(sim1, sim2):
+            self.assertAlmostEqual(v1, v2, places=10)
+
+    def test_densityprofile(self):
+        N = 123
+        z, d = self.model.densityprofile(N)
+        self.assertEqual(len(z), N)
+        self.assertEqual(len(d), N)
+
+
+if __name__ == '__main__':
+    unittest.main()
