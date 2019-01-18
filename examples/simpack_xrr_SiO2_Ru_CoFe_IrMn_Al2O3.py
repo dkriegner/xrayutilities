56c174b6b7140cbfb4aff03ec51965b71d28bfd1
Dominik Kriegner <dominik.kriegner@gmail.com>
2019-01-18 19:22:23 +0100

    update example of reflectivity fitting (FitModel)

--- examples/simpack_xrr_SiO2_Ru_CoFe_IrMn_Al2O3.py
+++ examples/simpack_xrr_SiO2_Ru_CoFe_IrMn_Al2O3.py
@@ -29,47 +29,31 @@ ai /= 2.0
 
 # define layers
 # SiO2 / Ru(5) / CoFe(3) / IrMn(3) / AlOx(10)
-lSiO2 = xu.simpack.Layer(xu.materials.SiO2, inf)
-lRu = xu.simpack.Layer(xu.materials.Ru, 50)
+lSiO2 = xu.simpack.Layer(xu.materials.SiO2, inf, roughness=2.5)
+lRu = xu.simpack.Layer(xu.materials.Ru, 47, roughness=2.8)
 rho_cf = 0.5*8900 + 0.5*7874
 mat_cf = xu.materials.Amorphous('CoFe', rho_cf)
-lCoFe = xu.simpack.Layer(mat_cf, 30)
-lIrMn = xu.simpack.Layer(xu.materials.Ir20Mn80, 30)
-lAl2O3 = xu.simpack.Layer(xu.materials.Al2O3, 100)
+lCoFe = xu.simpack.Layer(mat_cf, 27, roughness=4.6)
+lIrMn = xu.simpack.Layer(xu.materials.Ir20Mn80, 21, roughness=3.0)
+lAl2O3 = xu.simpack.Layer(xu.materials.Al2O3, 100, roughness=5.5)
 
+# create model
 m = xu.simpack.SpecularReflectivityModel(lSiO2, lRu, lCoFe, lIrMn, lAl2O3,
-                                         energy='CuKa1')
+                                         energy='CuKa1', resolution_width=0.02,
+                                         sample_width=6, beam_width=0.25,
+                                         background=81, I0=6.35e9)
 
-p = lmfit.Parameters()
-#          (Name,                  Value,  Vary,   Min,  Max, Expr)
-p.add_many(('SiO2_thickness',  numpy.inf, False,  None, None, None),
-           ('SiO2_roughness',        2.5,  True,     0,    8, None),
-           ('Ru_thickness',         47.0,  True,    25,   70, None),
-           ('Ru_roughness',          2.8,  True,     0,    8, None),
-           ('Ru_density',            1.0,  True,   0.8,  1.0, None),
-           ('CoFe_thickness',       27.0,  True,    15,   50, None),
-           ('CoFe_roughness',        4.6,  True,     0,    8, None),
-           ('CoFe_density',          1.0,  True,   0.8,  1.2, None),
-           ('Ir20Mn80_thickness',   21.0,  True,    15,   40, None),
-           ('Ir20Mn80_roughness',    3.0,  True,     0,    8, None),
-           ('Ir20Mn80_density',      1.1,  True,   0.8,  1.2, None),
-           ('Al2O3_thickness',     100.0,  True,    70,  130, None),
-           ('Al2O3_roughness',       5.5,  True,     0,    8, None),
-           ('Al2O3_density',         1.0,  True,   0.8,  1.2, None),
-           # primary beam intensity
-           ('I0',                 6.75e9,  True,   3e9,  8e9, None),
-           # background level of the measurement
-           ('background',             81,  True,    40,  100, None),
-           # zero shift of the incidence angle
-           ('shift',                   0,  True, -0.02, 0.02, None),
-           # size of the sample along the beam propagation direction
-           ('sample_width',          6.0, False,     2,    8, None),
-           # width of the beam in the reflection plane
-           ('beam_width',           0.25, False,   0.2,  0.4, None),
-           ('resolution_width',     0.02, False,  0.01, 0.05, None))
+# embed model in fit code
+fitm = xu.simpack.FitModel(m, plot=True, verbose=True)
 
-res = xu.simpack.fit_xrr(m, p, ai, data=edata, eps=eps, xmin=0.05, xmax=8.0,
-                         plot=True, verbose=True)
+# set some parameter limitations
+fitm.set_param_hint('SiO2_density', vary=False)
+fitm.set_param_hint('Al2O3_density', min=0.8, max=1.2)
+p = fitm.make_params()
+fitm.set_fit_limits(xmin=0.05, xmax=8.0)
+
+# perform the fit
+res = fitm.fit(edata, p, ai, weights=1/eps)
 lmfit.report_fit(res, min_correl=0.5)
 
 m.densityprofile(500, plot=True)
