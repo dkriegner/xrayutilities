a8d05c113067b0eeb62a06ae6bd5a1495c95a585
Dominik Kriegner <dominik.kriegner@gmail.com>
2021-07-14 09:28:51 +0200

    update copyright lines in tests

--- tests/test_io_specsardana.py
+++ tests/test_io_specsardana.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 #
-# Copyright (C) 2019 Dominik Kriegner <dominik.kriegner@gmail.com>
+# Copyright (C) 2019-2020 Dominik Kriegner <dominik.kriegner@gmail.com>
 
 import math
 import os.path
