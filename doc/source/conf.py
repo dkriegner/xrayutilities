bd39c9b21b87d6ede0ac2bbb116617657012c411
Dominik Kriegner <dominik.kriegner@gmail.com>
2022-10-31 11:00:12 +0100

    pump version to 1.7.4, update copyright in docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'xrayutilities'
-copyright = '2021, Dominik Kriegner, Eugen Wintersberger'
+copyright = '2022, Dominik Kriegner, Eugen Wintersberger'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
