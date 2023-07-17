776a66da64689b987a52179de7b5145c29630d1c
Dominik Kriegner <dominik.kriegner@gmail.com>
2023-07-17 11:08:34 +0200

    update copyright statement in new example

--- examples/simpack_xrd_dyn_AlGaAs.py
+++ examples/simpack_xrd_dyn_AlGaAs.py
@@ -13,7 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 #
-# Copyright (c) 2016-2023 Dominik Kriegner <dominik.kriegner@gmail.com>
 # Copyright (c) 2023 C2N - CNRS
 # Author:	  Aristide Lemaître <aristide.lemaitre@c2n.upsaclay.fr>
 
