dcc33f1ee9c874e3f16323fef58744f06cba8841
Dominik Kriegner <dominik.kriegner@gmail.com>
2024-04-09 10:35:09 +0200

    fix typo in setup.py (missing comma)

--- setup.py
+++ setup.py
@@ -148,7 +148,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12"
+        "Programming Language :: Python :: 3.12",
         "Topic :: Scientific/Engineering :: Physics",
         "Intended Audience :: Science/Research",
         "Development Status :: 5 - Production/Stable",
